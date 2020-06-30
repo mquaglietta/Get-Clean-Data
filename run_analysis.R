22242340b7f4770148dda00e2bf992a0bdc98efe
mquaglietta <61231852+mquaglietta@users.noreply.github.com>
2020-06-30 10:43:36 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,36 +1,37 @@
+library(dplyr)
+
 # Read Datasets
 X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+Subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
 Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
+Subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
 # Merges the training and the test sets to create one data set
 X <- rbind(X_test, X_train)
 Y <- rbind(Y_test, Y_train)
+Subjects <- rbind(Subject_test, Subject_train)
 
-colnames(Y) <- c("Y")
-XY <- cbind(X, Y)
+XYSubjects <- cbind(X, Subjects, Y)
 
-# Clear Environment
-rm(X,Y, X_test, X_train, Y_test, Y_train)
+rm(X,Y, X_test, X_train, Y_test, Y_train, Subject_test, Subject_train, Subjects) # Clear Environment
 
 # Extracting values for mean and std
 features <- read.table("./UCI HAR Dataset/features.txt", colClasses = "character")
-list_of_features <- c(grep("mean()", features$V2, fixed = TRUE),
-                      grep("std()", features$V2, fixed = TRUE))
-list_of_features <- sort(list_of_features)
+features_index <- grep("mean()|std()", features$V2)
 
-XY_cleaned <- XY[c(list_of_features, 562)]
-rm(XY)
+XYSubjects_cleaned <- XYSubjects[c(features_index, 562, 563)]
+
+rm(XYSubjects) # Clear Environment
+
+#Appropriately labels the data set with descriptive variable names.
+colnames(XYSubjects_cleaned) <- c(features$V2[features_index], "Subject", "Activity")
 
 # Assign activity labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
-XY_cleaned$Y <- activity_labels$V2[XY_cleaned$Y]
+XYSubjects_cleaned$Activity <- activity_labels$V2[XYSubjects_cleaned$Activity]
 
-#Appropriately labels the data set with descriptive variable names.
-colnames(XY_cleaned) <- c(features$V2[list_of_features], "Activity")
-
-library(dplyr)
-new_dataset <- tbl_df(XY_cleaned)
-new_dataset <- new_dataset %>% group_by(Activity) %>% summarise(across(1:66, mean))
+new_dataset <- tbl_df(XYSubjects_cleaned)
+new_dataset <- new_dataset %>% group_by(Subject, Activity) %>% summarise(across(1:66, mean))
 write.table(new_dataset, "tidy.txt", row.name=FALSE) 
